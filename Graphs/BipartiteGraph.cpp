f12a554587770ec04aa32445c77b48eb80d82d56
Najamulhassan3383 <najmulhassan721@gmail.com>
2023-07-17 07:31:03 +0500

    added the problem to check BipartiteGraph

---
+++ Graphs/BipartiteGraph.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include "./Graphs.h"
+using namespace std;
+bool CheckBipartite(int node, vector<int> &color, vector<vector<int>> &list)
+{
+    queue<int> q;
+    q.push(node);
+
+    color[node] = 0;
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        for (int neighbor : list[node])
+        {
+            if (color[neighbor] == -1)
+            {
+                color[neighbor] = !color[node];
+
+                q.push(neighbor);
+            }
+            else if (color[neighbor] == color[node])
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int nodes, edges;
+    cin >> nodes >> edges;
+    Graphs graph(nodes, edges);
+    graph.buildGraphList();
+
+    vector<int> color(nodes + 1, -1);
+    vector<bool> visited(nodes + 1, false);
+    vector<vector<int>> adjList = graph.GetList();
+
+    cout << CheckBipartite(0, color, adjList);
+
+    return 0;
+}
